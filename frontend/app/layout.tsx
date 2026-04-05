20713fe0055d9a19f84816d4a5df0e588ec1d493
minori-wakade <minoriwakade@gmail.com>
2026-04-05 10:26:19 +0530

    Fix frontend folder (remove submodule)

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "next-themes";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Signalist",
+  description: "Track real-time stock prices, get personalised alerts and explore detailed company insights.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+  <html lang="en" suppressHydrationWarning>
+    <body className="min-h-full flex flex-col">
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        {children}
+      </ThemeProvider>
+    </body>
+  </html>
+);
+}
